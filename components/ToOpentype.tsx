7b54b5a874e79a8aa2164bf2e3907e2a95d063bf
Solaiman Kmail <psokmail@gmail.com>
2022-07-29 02:09:06 +0200

    download

--- components/ToOpentype.tsx
+++ components/ToOpentype.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import useFresh from "../hooks/useFresh";
 import { Font } from "../types";
 import toOpentype from "../utils/toOpentype";
+import Button from "./Button";
 
 export default function ToOpenType({
   font,
@@ -10,18 +11,13 @@ export default function ToOpenType({
   font: Font;
   selected: string;
 }) {
-  const [fontStyle, setFontStyle] = useState("");
   const [testData, setTestData] = useState<string>();
   const [isChanged, setIsChanged] = useState(false);
-
+  const [fontUrl, setFontUrl] = useState("");
   useEffect(() => {
     const url = toOpentype(font);
 
-    const style = `
-    @font-face {font-family: "XXX";src: url("${url}") format("opentype");}
-    `;
-
-    setFontStyle(style);
+    setFontUrl(url);
   }, [font]);
 
   const getIsChanged = useFresh(isChanged);
@@ -31,37 +27,53 @@ export default function ToOpenType({
       setTestData(font.glyphs.items[selected].string);
     }
   }, [selected, font, testData]);
+  if (!fontUrl) {
+    return null;
+  }
   return (
     <>
-      {!!fontStyle && (
-        <>
-          <style
-            dangerouslySetInnerHTML={{
-              __html: fontStyle,
-            }}
-          ></style>
+      <Button
+        onClick={() => {
+          const familyName = font.familyName;
+          const styleName = font.subfamilyName;
+          const fileName =
+            familyName.replace(/\s/g, "") + "-" + styleName + ".otf";
+          const link = document.createElement("a");
+          link.href = fontUrl;
+          link.download = fileName;
+          link.click();
+        }}
+      >
+        Download
+      </Button>
 
-          <div className="flex flex-col items-start relative">
-            <span  className="text-sm uppercase mb-1 px-2 py-0.5 mb-2 bg-red-500 text-white absolute top-2 left-2">
-              Font testing
-            </span>
-            <textarea
-              style={{
-                fontFamily: "'XXX'",
-              }}
-              value={testData}
-              onChange={(e) => {
-                setIsChanged(
-                  e.target.value !== font.glyphs.items[selected].string
-                );
-                setTestData(e.target.value);
-              }}
-              placeholder="Test your data here"
-              className="h-24 text-2xl ring focus:outline-none ring-zinc-800 rounded p-2 shadouw  w-64 bg-white pt-9 "
-            ></textarea>
-          </div>
-        </>
-      )}
+      <>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `@font-face {font-family: "XXX";src: url("${fontUrl}") format("opentype");}`,
+          }}
+        ></style>
+
+        <div className="flex flex-col items-start relative">
+          <span className="text-sm uppercase mb-1 px-2 py-0.5 mb-2 bg-red-500 text-white absolute top-2 left-2">
+            Font testing
+          </span>
+          <textarea
+            style={{
+              fontFamily: "'XXX'",
+            }}
+            value={testData}
+            onChange={(e) => {
+              setIsChanged(
+                e.target.value !== font.glyphs.items[selected].string
+              );
+              setTestData(e.target.value);
+            }}
+            placeholder="Test your data here"
+            className="h-24 text-2xl ring focus:outline-none ring-zinc-800 rounded p-2 shadouw  w-64 bg-white pt-9 "
+          ></textarea>
+        </div>
+      </>
     </>
   );
 }
